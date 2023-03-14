d1f229b7088a0303aabda19dc689ad33604de610
Robin Schmidt <schmidt.rbn@gmail.com>
2023-03-14 16:19:02 +0100

    MODIFY added documentation

--- src/arrayWrapper/ArrayWrapper.java
+++ src/arrayWrapper/ArrayWrapper.java
@@ -8,10 +8,18 @@ public class ArrayWrapper {
         this.array = new int[arraySize];
     }
 
+    /**
+     * Method to return current array size
+     * @return current array size
+     */
     public int getArraySize() {
         return this.array.length;
     }
 
+    /**
+     * Method to add a new element to array. If array is full array size is doubled
+     * @param element to add
+     */
      public void add(int element) {
         if (this.elementCount >= this.array.length) {
             int[] newArray = new int[this.array.length * 2];
@@ -21,10 +29,19 @@ public class ArrayWrapper {
         array[elementCount++] = element;
     }
 
+    /**
+     * Method to get access to element as specific index
+     * @param index
+     * @return integer value of interest
+     */
     public int get(int index) {
         return array[index];
     }
 
+    /**
+     * Method to see how many elements the array currently holds
+     * @return current element in array
+     */
     public int getElementCount() {
         return elementCount;
     }
