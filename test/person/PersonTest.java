1d91a625e5fb68c3f74a57935d1a83b80be0441c
Robin Schmidt <schmidt.rbn@gmail.com>
2023-03-14 13:05:04 +0100

    MODIFY extended test with a additional testPerson

--- test/person/PersonTest.java
+++ test/person/PersonTest.java
@@ -7,14 +7,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class PersonTest {
 
     Person testPerson1 = new Person(38, "Schotte", "Heiko", 1.78, 80.3);
-    Person testPerson2 = new Person(88, "Schulz", "Horst", 1.68, 75.5);
+    Person testPerson2 = new Person(38, "Schotte", "Heiko", 1.78, 80.3);
+    Person testPerson3 = new Person(88, "Schulz", "Horst", 1.68, 75.5);
 
     @Test
     void personIsEqual() {
-        assertEquals(testPerson1, testPerson1);
+        assertEquals(testPerson1, testPerson2);
     }
     @Test
     void personIsNotEqual() {
-        assertNotEquals(testPerson1, testPerson2);
+        assertNotEquals(testPerson1, testPerson3);
     }
 }
