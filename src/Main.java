76ea517ed59789ec0b981ae1df420f3e161b7d00
Robin Schmidt <schmidt.rbn@gmail.com>
2023-03-15 14:27:02 +0100

    ADD created ObjectCopier for shallow copies with non-generic and generic solution

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,22 @@
+import copyingObjects.ObjectCopier;
+import person.Person;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
-        for (int i = 0; i < 10; i++ ){
-            if (i == 3){
-                break;
-                //return;
-                //continue;
-            } else {
-                System.out.println(i);
-            }
-        }
+        Person HansPeter = new Person(65, "Geerdes", "Hans Peter", 1.86, 82.3);
+
+        //List<Person> shallowCopies = ObjectCopier.shallowCopy(HansPeter, 6);
+        //System.out.println(shallowCopies);
+
+        List<Person> genericShallowCopies = ObjectCopier.genericShallowCopy(HansPeter, 6);
+        System.out.println(genericShallowCopies);
+
+        genericShallowCopies.get(0).setAge(70);
+        System.out.println(genericShallowCopies);
     }
 }
---
+++ src/copyingObjects/ObjectCopier.java
@@ -0,0 +1,46 @@
+package copyingObjects;
+
+import person.Person;
+
+import java.util.*;
+
+public class ObjectCopier {
+
+    /**
+     * Creates a List of shallow copies of a Person object
+     * @param person object which will be copied
+     * @param numberOfCopies defines how many shallow copies are made to fill the List with
+     * @return a List of shallow copies of a Person object
+     */
+    public static List<Person> shallowCopy(Person person, int numberOfCopies ) {
+
+        List<Person> shallowCopies = new ArrayList<>();
+
+        for (int i = 1; i <= numberOfCopies; i++) {
+            Person copy = person;
+            shallowCopies.add(copy);
+        }
+
+        return shallowCopies;
+    }
+
+    /**
+     * Creates a List of shallow copies of a generic object
+     * @param o object which will be copied
+     * @param numberOfCopies defines how many shallow copies are made to fill the List with
+     * @return a List of shallow copies of a Person object
+     * @param <T> generic type which is excepted
+     */
+    public static <T> List<T> genericShallowCopy(T o, int numberOfCopies ) {
+
+        List<T> shallowCopies = new ArrayList<>();
+
+        for (int i = 1; i <= numberOfCopies; i++) {
+            T copy = o;
+            shallowCopies.add(copy);
+        }
+
+        return shallowCopies;
+    }
+
+}
