04a16ee9f1fbfff590eb830a334fc0241cff2d3d
Robin Schmidt <schmidt.rbn@gmail.com>
2023-03-16 11:10:17 +0100

    ADD PersonHeightComparator implementing Comparator interface

--- src/person/PersonHeightComparator.java
+++ src/person/PersonHeightComparator.java
@@ -2,12 +2,12 @@ package person;
 
 import java.util.Comparator;
 
-public class PersonAgeComparator implements Comparator<Person> {
+public class PersonHeightComparator implements Comparator<Person> {
     @Override
     public int compare(Person o1, Person o2) {
-        if (o1.getAge() < o2.getAge()) {
+        if (o1.getHeight() < o2.getHeight()) {
             return -1;
-        } else if (o1.getAge() > o2.getAge()) {
+        } else if (o1.getHeight() > o2.getHeight()) {
             return 1;
         } else {
             return 0;
